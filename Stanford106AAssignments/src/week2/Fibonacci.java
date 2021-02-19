596238e06c8308f1834f5b4eb43694411e3ee2ee
Johnelee Dizon <johnelee.fl@gmail.com>
2021-02-19 13:59:52 -0500

    First commit

---
+++ Stanford106AAssignments/src/week2/Fibonacci.java
@@ -0,0 +1,22 @@
+package week2;
+
+import acm.program.*;
+
+public class Fibonacci extends ConsoleProgram {
+	
+	//Max term value must not exceed 10,000.
+	private static final int MAX_TERM_VALUE = 10000;
+	
+	public void run() {
+	println("This program lists the Fibonacci Sequence:");
+	
+		int Fib = 0;
+		int Fib1 = 1;
+		while (Fib <= MAX_TERM_VALUE) {	
+			println(Fib);					
+			int Fib2 = Fib + Fib1;			
+			Fib  = Fib1;					
+			Fib1 = Fib2;					
+			}
+	}
+}
