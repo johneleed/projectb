e9057e49a64e6a518f082681b786c5d934ba1404
Johnelee Dizon <johnelee.fl@gmail.com>
2021-02-22 08:47:45 -0500

    commit drawinglines.java

--- Stanford106AAssignments/src/week2/RandomCircles.java
+++ Stanford106AAssignments/src/week2/RandomCircles.java
@@ -2,26 +2,32 @@
 
 package week2;
 
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
-import acm.graphics.GOval;
+import acm.program.*;
+import acm.util.*;
+import acm.graphics.*;
 
 public class RandomCircles extends GraphicsProgram {
 
-private static final int NOC = 10;
-private static final double MIN_RADIUS = 5;
-private static final douvle MAX_RADIUS = 50;
+	//Number of circles
+	private static final int NOC = 10;
 
-	public void run() {
+	//Minimum radius
+	private static final double MIN_RADIUS = 5;
+
+	//Maximum radius
+	private static final double MAX_RADIUS = 50;
 		
-		RandomGenerator rgen = new RandomGenerator ();
-		for (int i = 0; i < NOC; i++) {
-		
-			int x = rgen.nextInt(50, 450);
-			int y = rgen.nextInt(50, 350);
-			int r = rgen.nextInt(5, 50);
-			
-				GOval circle = new GOval (x, y, r, r);
-				add (circle);	
+		public void run() {
+			for (int i = 0; i < NOC; i++) {
+			double r = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+			double x = rgen.nextDouble(0, getWidth() - 2 * r);
+			double y = rgen.nextDouble(0, getHeight() - 2 * r);
+			GOval circle = new GOval(x, y, 2 * r, 2 * r);
+			circle.setFilled(true);
+			circle.setColor(rgen.nextColor());
+			add(circle);
+		}
 	}
+// Private instance variable
+private RandomGenerator rgen = RandomGenerator.getInstance();
 }
--- Stanford106AAssignments/src/week2/RobotFace.java
+++ Stanford106AAssignments/src/week2/RobotFace.java
@@ -1,7 +1,59 @@
+//Draw a robot-looking face.
+
 package week2;
 
-import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
 
 public class RobotFace extends GraphicsProgram {
 
+	//Parameters for the drawing
+	private static final int HEAD_WIDTH = 150;	//Head is a big rectangle
+	private static final int HEAD_HEIGHT = 250; 
+	private static final int EYE_RADIUS = 20;	//Eyes are circles.
+	private static final int MOUTH_WIDTH = 100;  //Mouth is a rectangle.
+	private static final int MOUTH_HEIGHT = 30; 
+
+    public void run() {
+        buildHead();
+        buildLeftEye();
+        buildRightEye();
+        buildMouth();
+    }
+    private void buildHead() {
+        double startingWidth = ((getWidth()/2) - (HEAD_WIDTH/2));
+        double startingHeight = ((getHeight()/2) - (HEAD_HEIGHT/2));
+        GRect face = new GRect (startingWidth, startingHeight, HEAD_WIDTH, HEAD_HEIGHT);
+        add(face);
+        face.setFilled(true);
+        face.setFillColor(Color.GRAY);
+    }
+    private void buildLeftEye() {
+        double startingWidth = getWidth()/2 - HEAD_WIDTH/4 - EYE_RADIUS/2;
+        double startingHeight = getHeight()/2 - HEAD_HEIGHT/4 - EYE_RADIUS/2;
+        GOval leftEye = new GOval (startingWidth, startingHeight, EYE_RADIUS, EYE_RADIUS);
+        add(leftEye);
+        leftEye.setColor(Color.YELLOW);
+        leftEye.setFilled(true);
+        leftEye.setFillColor(Color.YELLOW);
+    }
+    private void buildRightEye() {
+        double startingWidth = getWidth()/2 + HEAD_WIDTH/4 - EYE_RADIUS/2;
+        double startingHeight = getHeight()/2 - HEAD_HEIGHT/4 - EYE_RADIUS/2;
+        GOval rightEye = new GOval (startingWidth, startingHeight, EYE_RADIUS, EYE_RADIUS);
+        add(rightEye);
+        rightEye.setColor(Color.YELLOW);
+        rightEye.setFilled(true);
+        rightEye.setFillColor(Color.YELLOW);
+    }
+    private void buildMouth() {
+        double startingWidth = getWidth()/2 - MOUTH_WIDTH/2;
+        double startingHeight = getHeight()/2 + HEAD_HEIGHT/4 - MOUTH_HEIGHT/2;
+        GRect mouth = new GRect (startingWidth, startingHeight, MOUTH_WIDTH, MOUTH_HEIGHT);
+        add(mouth);
+        mouth.setColor(Color.WHITE);
+        mouth.setFilled(true);
+        mouth.setFillColor(Color.WHITE);
+    }
 }
